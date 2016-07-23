95970ead680f3c44755bc3e3671fb187ac1c3e27
Anna Chepiga <anna.chepiga@list.ru>
2016-07-23 12:40:25 +0300

    Module 10 task completed

--- src/main/java/ua/goit/gojava/module_10/Runner.java
+++ src/main/java/ua/goit/gojava/module_10/Runner.java
@@ -1,4 +1,45 @@
 package ua.goit.gojava.module_10;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Runner {
+    private static String workingDir = System.getProperty("user.dir");
+
+    public static void main(String[] args) throws IOException {
+        String filename = "NewFile.txt";
+        String absolutePath = workingDir + File.separator + filename;
+        String content;
+        int key = 5;
+
+        File file = new File(absolutePath);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        System.out.println("Please enter the text to encrypt and write into the file:");
+
+        Scanner sc = new Scanner(System.in);
+
+        content = sc.nextLine();
+
+        try (DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(absolutePath)))) {
+            String encrypted = CesarAlgorithm.encrypt(content, key);
+            out.writeUTF(encrypted);
+            
+            System.out.println("\nThe text is encoded and written to NewFile.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File does not exist");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(absolutePath)))) {
+            String readText = in.readUTF();
+            String decrypted = CesarAlgorithm.decrypt(readText, key);
+
+            System.out.println("\nHere is the decrypted text:\n" + decrypted);
+        }
+    }
 }
